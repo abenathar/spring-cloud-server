bf2e4161e47c70786e8ff09ab69cc1c7504a873e
Eduardo Aben Athar <eduardo@abenathar.com.br>
2022-03-02 10:51:18 -0300

    Add new files

---
+++ src/test/java/br/com/abenathar/cloudserver/SpringCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abenathar.cloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
